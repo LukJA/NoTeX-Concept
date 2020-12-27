40d2a014db53853810315afaa99c70730d439b5a
Luke Andrews <l.j.andrews.uk@gmail.com>
2020-12-27 16:46:52 +0000

    Configure an example pip and setuptools package

---
+++ pack_tutorial/setup.py
@@ -0,0 +1,26 @@
+## setup.py is the build script for setuptools. It tells setuptools about your 
+## package (such as the name and version) as well as which code files to include.
+
+import setuptools
+
+# Use the README file as the long description content
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-LukJA", # Unique PyPI Package Name
+    version="0.0.1",
+    author="Luke Andrews",
+    author_email="l.j.andrews.uk@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LukJA/NoTeX-Concept",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
